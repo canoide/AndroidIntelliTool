9e2a2ab4420568b01f0985a2ec5e812b6e5e5c6a
Jorge Cano <jorgecano017@gmail.com>
2025-10-28 10:57:52 -0300

    Fixed open url.

--- AndroidIntelliTool/AboutForm.cs
+++ AndroidIntelliTool/AboutForm.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
 
@@ -8,8 +9,20 @@ namespace AndroidIntelliTool
         public AboutForm()
         {
             InitializeComponent();
-            this.githubLinkLabel.LinkClicked += (s, e) => Process.Start("https://github.com/canoide");
-            this.emailLinkLabel.LinkClicked += (s, e) => Process.Start("mailto:jorgecano017@gmail.com");
+            this.githubLinkLabel.LinkClicked += (s, e) => OpenUrl("https://github.com/canoide");
+            this.emailLinkLabel.LinkClicked += (s, e) => OpenUrl("mailto:jorgecano017@gmail.com");
+        }
+
+        private void OpenUrl(string url)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while trying to open the URL: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
